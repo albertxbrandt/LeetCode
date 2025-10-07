f9fb618a8c5751bf066a5ced91a87ae60db15b44
Albert <albert@thebrandtfamily.us>
2025-10-07 12:39:04 -0700

    Daily Problem 10-6

---
+++ Easy/single-number.py
@@ -0,0 +1,13 @@
+# Problem: Neetcode150 (https://neetcode.io/problems/single-number?list=neetcode150)
+# Thought Process: For every number that appears twice they need to be cancelled out. Leaving a single number not cancelled out.
+
+class Solution:
+    def singleNumber(self, nums: list[int]) -> int:
+        result = 0 # Store result of XOR operation
+        for num in nums:
+            result ^= num # Use XOR to cancel out numbers appearing twice because it goes by the bits
+        return result
+    
+sol = Solution()
+print(sol.singleNumber([2,2,1,5,5,6,6]))  # Output: 1
+print(sol.singleNumber([60,5,3,5,60]))  # Output: 3
