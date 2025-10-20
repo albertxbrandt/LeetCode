4b2b363dbd53c582ba71f6441ee55563d5f87bfb
Albert <albert@thebrandtfamily.us>
2025-10-20 15:44:35 -0700

    10-20

--- Easy/reverse-bits.py
+++ Easy/reverse-bits.py
@@ -2,6 +2,7 @@
 # Thought process: The problem is to reverse the bits of a given 32 bits unsigned integer.
 # To achieve this, we can iterate through each of the 32 bits of the integer.
 # We check if each is 0 or 1 and then set the corresponding bit in the result integer at the reversed position.
+
 class Solution:
     def reverseBits(self, n: int) -> int:
         result = 0
